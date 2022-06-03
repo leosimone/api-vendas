2209d7ee79247ee0eb0bf25b1058e75c4f0a0def
Leonardo Simone <leosimone@gmail.com>
2022-06-03 15:33:53 -0300

    Commit

---
+++ src/modules/products/typeorm/entities/services/UpdateProductService.ts
@@ -0,0 +1,44 @@
+import AppError from '@shared/errors/AppError';
+import { Interface } from 'readline';
+import { getCustomRepository } from 'typeorm';
+import Product from '../Product';
+import { ProductRepository } from '../repositories/ProductsRepository';
+
+interface IRequest {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class UpdateProductService {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product nodt found');
+    }
+
+    const productExists = await productsRepository.findByName(name);
+    if (productExists && name != product.name) {
+      throw new AppError('There is already a product with this name');
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
+
+export default UpdateProductService;
