0ed556d357ee5d7f6b48eb2f20abae279c463ac7
Leonardo Simone <leosimone@gmail.com>
2022-06-06 16:57:32 -0300

    user service

---
+++ src/modules/products/users/typeorm/services/CreateUserService.ts
@@ -0,0 +1,32 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../entities/User';
+import UsersRepository from '../repositories/UserRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const emailExists = await usersRepository.findByEmail(email);
+
+    if (emailExists) {
+      throw new AppError('Email already used.');
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+export default CreateUserService;
