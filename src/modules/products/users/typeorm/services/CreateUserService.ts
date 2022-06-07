d3b4fef88303e4aa2456d2a937f5eb997f56b95f
Leonardo Simone <leosimone@gmail.com>
2022-06-07 11:31:50 -0300

    commit

--- src/modules/products/users/typeorm/services/CreateUserService.ts
+++ src/modules/products/users/typeorm/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import AppError from '@shared/errors/AppError';
+import { hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import User from '../entities/User';
 import UsersRepository from '../repositories/UserRepository';
@@ -18,10 +19,12 @@ class CreateUserService {
       throw new AppError('Email already used.');
     }
 
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await usersRepository.save(user);
