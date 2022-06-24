90189d326485eb1e07561a69e5bc149ba7567be2
Leonardo Simone <leosimone@gmail.com>
2022-06-24 08:31:13 -0300

    fix on upload

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -6,11 +6,13 @@ import routes from './routes';
 import AppError from '../errors/AppError';
 import '../typeorm';
 import { errors } from 'celebrate';
+import uploadConfig from '@config/upload';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use(errors());
