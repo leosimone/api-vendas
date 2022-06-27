71e82418bb9b031ed7983ba078c06f81c6080008
Leonardo Simone <leosimone@gmail.com>
2022-06-27 08:08:52 -0300

    commit 27JUN

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,23 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UserRepository';
+import UserTokenRepository from '../typeorm/repositories/UserTokenRepository';
+
+interface IRequest {
+  email: string;
+}
+
+class SendForgotPasswordEmailService {
+  public async execute({ email }: IRequest): Promise<void> {
+    //void, não vai precisar retornar nada, só enviar o email
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokenRepository = getCustomRepository(UserTokenRepository);
+
+    const user = await usersRepository.findByEmail(email);
+    if (!user) {
+      throw new AppError('User does not exists');
+    }
+    const token = await userTokenRepository.generate(user.id);
+  }
+}
+export default SendForgotPasswordEmailService;
