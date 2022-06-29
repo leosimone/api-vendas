bf113143f238e139aee43d20ac1c70cf31efac2b
Leonardo <leosimone@gmail.com>
2022-06-28 21:34:13 -0300

    fix on forgot email

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,41 @@
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UserRepository';
+import UserTokenRepository from '../typeorm/repositories/UserTokenRepository';
+import AppError from '@shared/errors/AppError';
+// eslint-disable-next-line prettier/prettier
+import {isAfter, addHours} from 'date-fns';
+// eslint-disable-next-line prettier/prettier
+import {hash} from 'bcryptjs';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+class ResetPasswordService {
+  public async execute({ token, password }: IRequest): Promise<void> {
+    //void, não vai precisar retornar nada, só enviar o email
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokenRepository = getCustomRepository(UserTokenRepository);
+
+    const userToken = await userTokenRepository.findByToken(token);
+    if (!userToken) {
+      throw new AppError('User token does not exists');
+    }
+
+    const user = await usersRepository.findById(userToken.user_id);
+    if (!user) {
+      throw new AppError('User does not exists');
+    }
+
+    const tokenCreatedAt = userToken.created_at;
+    const compareDate = addHours(tokenCreatedAt, 2);
+    // eslint-disable-next-line prettier/prettier
+    if(isAfter(Date.now(), compareDate)){
+      throw new AppError('token expired');
+    }
+
+    user.password = await hash(password, 8);
+  }
+}
+export default ResetPasswordService;
