70b87e9a836ec3183cdbb88ad56361a5d95cf70d
Leonardo Simone <leosimone@gmail.com>
2022-07-01 15:56:04 -0300

    customer

---
+++ src/modules/customers/routes/customers.routes.ts
@@ -0,0 +1,58 @@
+import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+import CustomerController from '../controllers/CustomerController';
+import isAuthenticated from '@shared/http/middlewares/isAuthenticated';
+
+const customersRouter = Router();
+const customersController = new CustomerController();
+
+customersRouter.use(isAuthenticated);
+
+customersRouter.get('/', customersController.index);
+
+customersRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  customersController.show,
+);
+
+customersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+    },
+  }),
+  customersController.create,
+);
+
+customersRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  customersController.update,
+);
+
+customersRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  customersController.delete,
+);
+
+export default customersRouter;
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,3 +1,4 @@
+import customersRouter from '@modules/customers/routes/customers.routes';
 import productsRouter from '@modules/products/routes/products.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
@@ -12,5 +13,6 @@ routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
+routes.use('/customers', customersRouter);
 
 export default routes;
