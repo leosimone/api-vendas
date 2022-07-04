f6d0c033dc42297c992f7b1694072084a9c485e1
Leonardo Simone <leosimone@gmail.com>
2022-07-04 10:48:14 -0300

    typeorm

---
+++ src/modules/orders/typeorm/entities/Order.ts
@@ -0,0 +1,28 @@
+import Customer from '@modules/customers/typeorm/entities/Customer';
+import {
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('orders')
+class Order {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  //abaixo fica muitas orders para um customer
+  @ManyToOne(() => Customer)
+  @JoinColumn({ name: 'customer_id' })
+  customer: Customer;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Order;
