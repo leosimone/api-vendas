0ffb062d275ad9c1daff49e4b9d253f39cc85507
Leonardo Simone <leosimone@gmail.com>
2022-07-05 10:53:16 -0300

    inicio cache

---
+++ src/config/cache.ts
@@ -0,0 +1,20 @@
+//aula 101 inicio de cache com redis
+import { RedisOptions } from 'ioredis';
+
+interface ICacheConfig {
+  config: {
+    redis: RedisOptions;
+  };
+  driver: string;
+}
+
+export default {
+  config: {
+    redis: {
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PASS || undefined,
+    },
+  },
+  driver: 'redis',
+} as ICacheConfig;
