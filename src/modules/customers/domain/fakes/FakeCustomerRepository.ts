dc2348c79735a2299bba7ffe763574d41844af34
Leonardo Simone <leosimone@gmail.com>
2022-07-08 16:24:36 -0300

    jest

--- src/modules/customers/domain/fakes/FakeCustomerRepository.ts
+++ src/modules/customers/domain/fakes/FakeCustomerRepository.ts
@@ -21,7 +21,10 @@ class FakeCustomersRepository implements ICustomerRepository {
 
 
   public async save(customer: Customer): Promise<Customer> {
-    Object.assign(this.customers, customer);
+    const findIndex = this.customers.findIndex(
+      findCustomer => findCustomer.id === customer.id,
+    )
+    this.customers[findIndex] = customer;
     return customer;
   }
 
--- src/modules/customers/services/CreateCustomerService.spec.ts
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -1,11 +1,16 @@
+import AppError from '@shared/errors/AppError';
 import 'reflect-metadata';
 import FakeCustomersRepository from '../domain/fakes/FakeCustomerRepository';
 import CreateCustomerService from './CreateCustomerService';
 
+let fakeCustomersRepository: FakeCustomersRepository;
+let createCustomer: CreateCustomerService;
 describe('CreateCustomer', () => {
+  beforeEach(() => {
+    fakeCustomersRepository = new FakeCustomersRepository();
+    createCustomer = new CreateCustomerService(fakeCustomersRepository);
+  });
   it('should be able to create a new customer', async () => {
-    const fakeCustomersRepository = new FakeCustomersRepository();
-    const createCustomer = new CreateCustomerService(fakeCustomersRepository);
     const customer = await createCustomer.execute({
       name: 'Leo',
       email: 'leosimone@mail.com',
@@ -14,7 +19,16 @@ describe('CreateCustomer', () => {
     expect(customer).toHaveProperty('id');
   });
 
-  it('should not be able to create two customers with the same email', () => {
-    expect(1).toBe(1);
+  it('should not be able to create two customers with the same email', async () => {
+    await createCustomer.execute({
+      name: 'Leo',
+      email: 'leosimone@mail.com',
+    });
+    expect(
+      createCustomer.execute({
+        name: 'Leo',
+        email: 'leosimone@mail.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
