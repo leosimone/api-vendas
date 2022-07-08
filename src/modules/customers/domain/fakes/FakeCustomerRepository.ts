bfc00cd533573d601fd762eaca2eaa6f501d82db
Leonardo Simone <leosimone@gmail.com>
2022-07-08 13:16:32 -0300

    jest

---
+++ src/modules/customers/domain/fakes/FakeCustomerRepository.ts
@@ -0,0 +1,46 @@
+/* eslint-disable prettier/prettier */
+import { ICreateCustomer } from '@modules/customers/domain/interfaces/ICreateCustomer';
+import { ICustomerRepository } from '@modules/customers/domain/interfaces/repositories/ICustomersRepository';
+import Customer from '@modules/customers/infra/typeorm/entities/Customer';
+import { v4 as uuidv4 } from 'uuid';
+
+// refatorado na aula 136, onde foi alterado o repositorio
+class FakeCustomersRepository implements ICustomerRepository {
+  private customers: Customer[] = [];
+
+  public async create({ name, email }: ICreateCustomer): Promise<Customer> {
+    const customer = new Customer();
+    customer.id = uuidv4();
+    customer.name = name;
+    customer.email = email;
+
+    this.customers.push(customer);
+
+    return customer;
+  }
+
+
+  public async save(customer: Customer): Promise<Customer> {
+    Object.assign(this.customers, customer);
+    return customer;
+  }
+
+  public async findByName(name: string): Promise<Customer | undefined> {
+    const customer = this.customers.find(customer => customer.name === name);
+    return customer;
+  }
+
+  public async findById(id: string): Promise<Customer | undefined> {
+    const customer = this.customers.find(customer => customer.id === id);
+    return customer;
+  }
+
+
+  public async findByEmail(email: string): Promise<Customer | undefined> {
+    const customer = this.customers.find(customer => customer.email === email);
+    return customer;
+  }
+}
+
+
+export default FakeCustomersRepository;
--- src/modules/customers/services/CreateCustomerService.spec.ts
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -1,3 +1,20 @@
-test('should be 10', () => {
-  expect(5 + 5).toEqual;
+import 'reflect-metadata';
+import FakeCustomersRepository from '../domain/fakes/FakeCustomerRepository';
+import CreateCustomerService from './CreateCustomerService';
+
+describe('CreateCustomer', () => {
+  it('should be able to create a new customer', async () => {
+    const fakeCustomersRepository = new FakeCustomersRepository();
+    const createCustomer = new CreateCustomerService(fakeCustomersRepository);
+    const customer = await createCustomer.execute({
+      name: 'Leo',
+      email: 'leosimone@mail.com',
+    });
+
+    expect(customer).toHaveProperty('id');
+  });
+
+  it('should not be able to create two customers with the same email', () => {
+    expect(1).toBe(1);
+  });
 });
